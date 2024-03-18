2f0fcd5d1db59f8b994c6369c0b109f033eb530b
Michael Chiem <michaelchiem1@hotmail.com>
2024-03-18 08:29:40 -0600

    Updated Seed.sql File

--- seed.sql
+++ seed.sql
@@ -3,31 +3,31 @@ use employees;
 INSERT INTO department
     (name)
 VALUES
-    ('Sales'),
-    ('Engineering'),
-    ('Finance'),
-    ('Legal');
+    ('ProductionSupport'),
+    ('Shipping'),
+    ('Receiving'),
+    ('Maintenance');
 
 INSERT INTO role
     (title, salary, department_id)
 VALUES
-    ('Sales Lead', 100000, 1),
-    ('Salesperson', 80000, 1),
-    ('Lead Engineer', 150000, 2),
-    ('Software Engineer', 120000, 2),
-    ('Account Manager', 160000, 3),
-    ('Accountant', 125000, 3),
-    ('Legal Team Lead', 250000, 4),
-    ('Lawyer', 190000, 4);
+    ('ProductionSupport Manager', 100000, 1),
+    ('Receiving Manager', 80000, 1),
+    ('Shipping Manager', 150000, 2),
+    ('Maintenance Manager', 120000, 2),
+    ('ProductionSupport help desk', 160000, 3),
+    ('ForkLift Driver', 125000, 3),
+    ('Welder', 250000, 4),
+    ('Driver', 190000, 4);
 
 INSERT INTO employee
     (first_name, last_name, role_id, manager_id)
 VALUES
-    ('John', 'Doe', 1, NULL),
-    ('Mike', 'Chan', 2, 1),
-    ('Ashley', 'Rodriguez', 3, NULL),
-    ('Kevin', 'Tupik', 4, 3),
-    ('Kunal', 'Singh', 5, NULL),
-    ('Malia', 'Brown', 6, 5),
-    ('Sarah', 'Lourd', 7, NULL),
-    ('Tom', 'Allen', 8, 7);
+    ('Bob', 'Goodman', 1, NULL),
+    ('Big', 'Chin', 2, 1),
+    ('Wind', 'Horseshoes', 3, NULL),
+    ('Darell', 'Begay', 4, 3),
+    ('Denice', 'Lafeen', 5, NULL),
+    ('Malache', 'Boba', 6, 5),
+    ('Dave', 'BigChin', 7, NULL),
+    ('Peter', 'Griffin', 8, 7);
