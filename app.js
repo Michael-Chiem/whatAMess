cab0f10c83bb4c72598b704c34c42353c08882a8
Michael Chiem <michaelchiem1@hotmail.com>
2024-03-18 08:59:26 -0600

    Orignized Folders Structure

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const db = require('./connection'); // Assuming this file exports your database connection
+const db = require('./config/connection'); // Assuming this file exports your database connection
 
 const app = express();
 const port = 3000; // You can use any port you prefer
